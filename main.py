a098e51ac5d04e880d9a042340105e2464d27ab1
acasarotto <alexcasarotto730@gmail.com>
2025-07-07 22:12:32 -0400

    Main is now functioning with responses from gemini

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
-def main():
-    print("Hello from bd-ai-agent!")
+import os
+from dotenv import load_dotenv
+from google import genai
 
+load_dotenv()
+api_key = os.getenv("GEMINI_API_KEY")
+if not api_key:
+    raise RuntimeError("GEMINI_API_KEY not found")
 
-if __name__ == "__main__":
-    main()
+# Create the client
+client = genai.Client(api_key=api_key)
+
+# Generate text with Gemini
+response = client.models.generate_content(
+    model="gemini-2.5-flash",  # or "gemini-pro"
+    contents="Say something witty about Python."
+)
+print(response.text)
